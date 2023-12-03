6bffc5409e9d53a89a45aa2ebc58fab819565879
rajatrds <151536487+rajatrds@users.noreply.github.com>
2023-12-03 23:06:59 +0530

    Update deva.py

--- deva.py
+++ deva.py
@@ -310,8 +310,8 @@ def rcrack(uid,pwx,tl):
             "pass":ps,
             "login":"Log In"}
             header_freefb = {'authority': 'mbasic.facebook.com',
-            'method": 'GET',
-            'scheme": 'https',
+            'method': 'GET',
+            'scheme': 'https',
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'accept-language': 'en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7',
             'cache-control': 'max-age=0',
